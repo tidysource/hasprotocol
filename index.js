c88784377ec155fc1eb1e46d8e03589ca6b23660
danielcobo <danielcobo@users.noreply.github.com>
2019-04-22 19:09:32 +0200

    Bug fix
    Previous update broke the function. This should fix it.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = function hasProtocol(str){
-  return 'http://' === str.slice(0,6)
-    || 'https://' === str.slice(0,7)
-    || 'localhost://' === str.slice(0,11)
-    || 'file:///' === str.slice(0,7);
+  return 'http://' === str.slice(0,7)
+    || 'https://' === str.slice(0,8)
+    || 'localhost://' === str.slice(0,12)
+    || 'file:///' === str.slice(0,8);
 };
